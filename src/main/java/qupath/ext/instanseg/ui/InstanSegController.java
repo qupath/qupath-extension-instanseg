7d9b6841e27861f6ef9b877e9cc114623cabba04
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2024-04-22 15:20:26 +0100

    Missing import

--- src/main/java/qupath/ext/instanseg/ui/InstanSegController.java
+++ src/main/java/qupath/ext/instanseg/ui/InstanSegController.java
@@ -9,6 +9,7 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
