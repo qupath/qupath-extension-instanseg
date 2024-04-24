2b2ce18f7bd2329f01db46468dd00dcde35974a4
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2024-04-24 17:09:07 +0100

    Move code + classes around

--- src/main/java/qupath/ext/instanseg/ui/InstanSegController.java
+++ src/main/java/qupath/ext/instanseg/ui/InstanSegController.java
@@ -80,10 +80,6 @@ public class InstanSegController extends BorderPane {
 
     @FXML
     private CheckComboBox<ChannelSelectItem> comboChannels;
-
-    private final Watcher watcher = new Watcher();
-    private ExecutorService executor;
-
     @FXML
     private TextField tfModelDirectory;
     @FXML
@@ -107,6 +103,9 @@ public class InstanSegController extends BorderPane {
     private final QuPathGUI qupath = QuPathGUI.getInstance();
     private ObjectProperty<Task<?>> pendingTask = new SimpleObjectProperty<>();
     private MessageTextHelper messageTextHelper;
+
+    private final Watcher watcher = new Watcher(modelChoiceBox);
+    private ExecutorService executor;
 
     public static InstanSegController createInstance() throws IOException {
         return new InstanSegController();
@@ -138,10 +137,19 @@ public class InstanSegController extends BorderPane {
         updateChannelPicker(qupath.getImageData());
         qupath.imageDataProperty().addListener((v, o, n) -> updateChannelPicker(n));
         comboChannels.disableProperty().bind(qupath.imageDataProperty().isNull());
-        comboChannels.titleProperty().bind(Bindings.createStringBinding(() -> getTitle(comboChannels),
+        comboChannels.titleProperty().bind(Bindings.createStringBinding(() -> getCheckComboBoxText(comboChannels),
                 comboChannels.getCheckModel().getCheckedItems()));
         FXUtils.installSelectAllOrNoneMenu(comboChannels);
         addSetFromVisible(comboChannels);
+    }
+
+    private static String getCheckComboBoxText(CheckComboBox<ChannelSelectItem> comboBox) {
+        int n = comboBox.getCheckModel().getCheckedItems().size();
+        if (n == 0)
+            return "No channels selected!";
+        if (n == 1)
+            return "1 channel selected";
+        return n + " channels selected";
     }
 
     private void addSetFromVisible(CheckComboBox<ChannelSelectItem> comboChannels) {
@@ -204,17 +212,6 @@ public class InstanSegController extends BorderPane {
         return list;
     }
 
-
-
-    private static String getTitle(CheckComboBox<ChannelSelectItem> comboBox) {
-        int n = comboBox.getCheckModel().getCheckedItems().size();
-        if (n == 0)
-            return "No channels selected!";
-        if (n == 1)
-            return "1 channel selected";
-        return n + " channels selected";
-    }
-
     private void configureThreadSpinner() {
         SpinnerValueFactory.IntegerSpinnerValueFactory factory = (SpinnerValueFactory.IntegerSpinnerValueFactory) threadSpinner.getValueFactory();
         factory.setMax(Runtime.getRuntime().availableProcessors());
@@ -226,7 +223,7 @@ public class InstanSegController extends BorderPane {
                 qupath.imageDataProperty().isNull()
                         .or(pendingTask.isNotNull())
                         .or(modelChoiceBox.getSelectionModel().selectedItemProperty().isNull())
-                        .or(messageTextHelper.warningText.isNotEmpty())
+                        .or(messageTextHelper.warningText().isNotEmpty())
                         .or(deviceChoices.getSelectionModel().selectedItemProperty().isNull())
         );
         pendingTask.addListener((observable, oldValue, newValue) -> {
@@ -270,9 +267,6 @@ public class InstanSegController extends BorderPane {
         watcher.interrupt();
     }
 
-    private void addListeners() {
-    }
-
     private void handleModelDirectory(String n) {
         if (n == null) return;
         var path = Path.of(n);
@@ -302,14 +296,14 @@ public class InstanSegController extends BorderPane {
     }
 
     private void configureMessageLabel() {
-        messageTextHelper = new MessageTextHelper();
-        labelMessage.textProperty().bind(messageTextHelper.messageLabelText);
-        if (messageTextHelper.hasWarning.get()) {
+        messageTextHelper = new MessageTextHelper(modelChoiceBox);
+        labelMessage.textProperty().bind(messageTextHelper.messageLabelText());
+        if (messageTextHelper.hasWarning().get()) {
             labelMessage.getStyleClass().setAll("warning-message");
         } else {
             labelMessage.getStyleClass().setAll("standard-message");
         }
-        messageTextHelper.hasWarning.addListener((observable, oldValue, newValue) -> {
+        messageTextHelper.hasWarning().addListener((observable, oldValue, newValue) -> {
             if (newValue)
                 labelMessage.getStyleClass().setAll("warning-message");
             else
@@ -324,7 +318,7 @@ public class InstanSegController extends BorderPane {
         if (!Files.exists(path)) return;
         try (var ps = Files.list(path)) {
             for (var file: ps.toList()) {
-                if (isValidModel(file)) {
+                if (InstanSegUtils.isValidModel(file)) {
                     box.getItems().add(InstanSegModel.createModel(file));
                 }
             }
@@ -337,112 +331,6 @@ public class InstanSegController extends BorderPane {
         executor = Executors.newSingleThreadExecutor();
         executor.submit(watcher::processEvents);
     }
-
-    class Watcher {
-        private static final Logger logger = LoggerFactory.getLogger(Watcher.class);
-
-        private final WatchService watchService;
-        private final Map<WatchKey, Path> keys;
-        private boolean interrupted;
-
-        @SuppressWarnings("unchecked")
-        static <T> WatchEvent<T> cast(WatchEvent<?> event) {
-            return (WatchEvent<T>)event;
-        }
-
-        private void register(Path dir) throws IOException {
-            WatchKey key = dir.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-            keys.put(key, dir);
-        }
-
-        private void unregister(Path dir) {
-            for (var es: keys.entrySet()) {
-                if (es.getValue().equals(dir)) {
-                    logger.debug("Unregister: {}", es.getValue());
-                    es.getKey().cancel();
-                    keys.remove(es.getKey());
-                }
-            }
-        }
-
-        /**
-         * Creates a WatchService and registers the given directory
-         */
-        Watcher() throws IOException {
-            this.watchService = FileSystems.getDefault().newWatchService();
-            this.keys = new ConcurrentHashMap<>();
-        }
-
-        /**
-         * Process all events for keys queued to the watcher
-         */
-        void processEvents() {
-            while (!interrupted) {
-
-                // wait for key to be signalled
-                WatchKey key;
-                try {
-                    key = watchService.take();
-                } catch (InterruptedException x) {
-                    return;
-                }
-
-                Path dir = keys.get(key);
-                if (dir == null) {
-                    logger.error("WatchKey not recognized!!");
-                    continue;
-                }
-
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    WatchEvent.Kind<?> kind = event.kind();
-                    WatchEvent<Path> ev = cast(event);
-                    Path name = ev.context();
-
-                    // Context for directory entry event is the file name of entry
-                    Path child = dir.resolve(name);
-
-                    // print out event
-                    logger.debug("{}: {}", event.kind().name(), child);
-
-                    if (kind == ENTRY_CREATE && isValidModel(name)) {
-                        try {
-                            modelChoiceBox.getItems().add(InstanSegModel.createModel(child));
-                        } catch (IOException e) {
-                            logger.error("Unable to add model", e);
-                        }
-                    }
-                    if (kind == ENTRY_DELETE && isValidModel(name)) {
-                        modelChoiceBox.getItems().removeIf(model -> model.getPath().equals(child));
-                    }
-
-                }
-
-                // reset key and remove from set if directory no longer accessible
-                boolean valid = key.reset();
-                if (!valid) {
-                    keys.remove(key);
-
-                    // all directories are inaccessible
-                    if (keys.isEmpty()) {
-                        break;
-                    }
-                }
-            }
-        }
-
-        void interrupt() {
-            interrupted = true;
-        }
-    }
-
-    private static boolean isValidModel(Path path) {
-        // return path.toString().endsWith(".pt"); // if just looking at pt files
-        if (Files.isDirectory(path)) {
-            return Files.exists(path.resolve("instanseg.pt")) && Files.exists(path.resolve("rdf.yaml"));
-        }
-        return false;
-    }
-
 
     @FXML
     private void runInstanSeg() {
@@ -476,179 +364,6 @@ public class InstanSegController extends BorderPane {
     @FXML
     private void selectAllTMACores() {
         QP.selectTMACores();
-    }
-
-    /**
-     * Helper class for determining which text to display in the message label.
-     */
-    private class MessageTextHelper {
-
-        private final SelectedObjectCounter selectedObjectCounter;
-
-        /**
-         * Text to display a warning (because inference can't be run)
-         */
-        private StringBinding warningText;
-        /**
-         * Text to display the number of selected objects (usually when inference can be run)
-         */
-        private StringBinding selectedObjectText;
-        /**
-         * Text to display in the message label (either the warning or the selected object text)
-         */
-        private StringBinding messageLabelText;
-
-        /**
-         * Binding to check if the warning is empty.
-         * Retained here because otherwise code that attaches a listener to {@code warningText.isEmpty()} would need to
-         * retain a reference to the binding to prevent garbage collection.
-         */
-        private BooleanBinding hasWarning;
-
-        MessageTextHelper() {
-            this.selectedObjectCounter = new SelectedObjectCounter(qupath.imageDataProperty());
-            configureMessageTextBindings();
-        }
-
-        private void configureMessageTextBindings() {
-            this.warningText = createWarningTextBinding();
-            this.selectedObjectText = createSelectedObjectTextBinding();
-            this.messageLabelText = Bindings.createStringBinding(() -> {
-                var warning = warningText.get();
-                if (warning == null || warning.isEmpty())
-                    return selectedObjectText.get();
-                else
-                    return warning;
-            }, warningText, selectedObjectText);
-            this.hasWarning = warningText.isEmpty().not();
-        }
-
-        private StringBinding createSelectedObjectTextBinding() {
-            return Bindings.createStringBinding(this::getSelectedObjectText,
-                    selectedObjectCounter.numSelectedAnnotations,
-                    selectedObjectCounter.numSelectedDetections);
-        }
-
-        private String getSelectedObjectText() {
-            int nAnnotations = selectedObjectCounter.numSelectedAnnotations.get();
-            int nDetections = selectedObjectCounter.numSelectedDetections.get();
-            int nCores = selectedObjectCounter.numSelectedTMACores.get();
-            if (nAnnotations == 1)
-                return resources.getString("ui.selection.annotations-single");
-            else if (nAnnotations > 1)
-                return String.format(resources.getString("ui.selection.annotations-multiple"), nAnnotations);
-            else if (nCores == 1)
-                return resources.getString("ui.selection.TMA-cores-single");
-            else if (nCores > 1)
-                return String.format(resources.getString("ui.selection.TMA-cores-multiple"), nCores);
-            else if (nDetections == 1)
-                return resources.getString("ui.selection.detections-single");
-            else if (nDetections > 1)
-                return String.format(resources.getString("ui.selection.detections-multiple"), nDetections);
-            else
-                return resources.getString("ui.selection.empty");
-        }
-
-        private StringBinding createWarningTextBinding() {
-            return Bindings.createStringBinding(this::getWarningText,
-                    qupath.imageDataProperty(),
-                    modelChoiceBox.getSelectionModel().selectedItemProperty(),
-                    selectedObjectCounter.numSelectedAnnotations,
-                    selectedObjectCounter.numSelectedTMACores,
-                    selectedObjectCounter.numSelectedDetections);
-        }
-
-        private String getWarningText() {
-            if (qupath.imageDataProperty().get() == null)
-                return resources.getString("ui.error.no-image");
-            if (modelChoiceBox.getSelectionModel().isEmpty())
-                return resources.getString("ui.error.no-model");
-            if (selectedObjectCounter.numSelectedAnnotations.get() == 0 &&
-                    selectedObjectCounter.numSelectedDetections.get() == 0 &&
-                    selectedObjectCounter.numSelectedTMACores.get() == 0)
-                return resources.getString("ui.error.no-selection");
-            return null;
-        }
-    }
-
-
-    /**
-     * Helper class for maintaining a count of selected annotations and detections,
-     * determined from an ImageData property (whose value may change).
-     * This addresses the awkwardness of attaching/detaching listeners.
-     */
-    private static class SelectedObjectCounter {
-
-        private final ObjectProperty<ImageData<?>> imageDataProperty = new SimpleObjectProperty<>();
-
-        private final PathObjectSelectionListener selectionListener = this::selectedPathObjectChanged;
-
-        private final ObservableValue<PathObjectHierarchy> hierarchyProperty;
-
-        private final IntegerProperty numSelectedAnnotations = new SimpleIntegerProperty();
-        private final IntegerProperty numSelectedDetections = new SimpleIntegerProperty();
-        private final IntegerProperty numSelectedTMACores = new SimpleIntegerProperty();
-
-        SelectedObjectCounter(ObservableValue<ImageData<BufferedImage>> imageDataProperty) {
-            this.imageDataProperty.bind(imageDataProperty);
-            this.hierarchyProperty = createHierarchyBinding();
-            hierarchyProperty.addListener((observable, oldValue, newValue) -> updateHierarchy(oldValue, newValue));
-            updateHierarchy(null, hierarchyProperty.getValue());
-        }
-
-        private ObjectBinding<PathObjectHierarchy> createHierarchyBinding() {
-            return Bindings.createObjectBinding(() -> {
-                        var imageData = imageDataProperty.get();
-                        return imageData == null ? null : imageData.getHierarchy();
-                    },
-                    imageDataProperty);
-        }
-
-        private void updateHierarchy(PathObjectHierarchy oldValue, PathObjectHierarchy newValue) {
-            if (oldValue == newValue)
-                return;
-            if (oldValue != null)
-                oldValue.getSelectionModel().removePathObjectSelectionListener(selectionListener);
-            if (newValue != null)
-                newValue.getSelectionModel().addPathObjectSelectionListener(selectionListener);
-            updateSelectedObjectCounts();
-        }
-
-        private void selectedPathObjectChanged(PathObject pathObjectSelected, PathObject previousObject, Collection<PathObject> allSelected) {
-            updateSelectedObjectCounts();
-        }
-
-        private void updateSelectedObjectCounts() {
-            if (!Platform.isFxApplicationThread()) {
-                Platform.runLater(this::updateSelectedObjectCounts);
-                return;
-            }
-            var hierarchy = hierarchyProperty.getValue();
-            if (hierarchy == null) {
-                numSelectedAnnotations.set(0);
-                numSelectedDetections.set(0);
-                numSelectedTMACores.set(0);
-                return;
-            }
-
-            var selected = hierarchy.getSelectionModel().getSelectedObjects();
-            numSelectedAnnotations.set(
-                    (int)selected
-                            .stream().filter(PathObject::isAnnotation)
-                            .count()
-            );
-            numSelectedDetections.set(
-                    (int)selected
-                            .stream().filter(PathObject::isDetection)
-                            .count()
-            );
-            numSelectedTMACores.set(
-                    (int)selected
-                            .stream().filter(PathObject::isTMACore)
-                            .count()
-            );
-        }
-
     }
 
     /**
@@ -692,31 +407,4 @@ public class InstanSegController extends BorderPane {
         dirPath.set(newDir.getAbsolutePath());
     }
 
-    static class ChannelSelectItem {
-        private String name;
-        private ColorTransforms.ColorTransform transform;
-
-        ChannelSelectItem(String name) {
-            this.name = name;
-            this.transform = ColorTransforms.createChannelExtractor(name);
-        }
-
-        ChannelSelectItem(String name, ColorTransforms.ColorTransform transform) {
-            this.name = name;
-            this.transform = transform;
-        }
-
-        @Override
-        public String toString() {
-            return this.name;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public ColorTransforms.ColorTransform getTransform() {
-            return transform;
-        }
-    }
 }
---
+++ src/main/java/qupath/ext/instanseg/ui/InstanSegUtils.java
@@ -0,0 +1,28 @@
+package qupath.ext.instanseg.ui;
+
+package qupath.ext.instanseg.core;
+
+import org.controlsfx.control.CheckComboBox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class InstanSegUtils {
+    private static final Logger logger = LoggerFactory.getLogger(InstanSegUtils.class);
+
+    private InstanSegUtils() {
+        throw new AssertionError("Do not instantiate this class");
+    }
+
+    public static boolean isValidModel(Path path) {
+        // return path.toString().endsWith(".pt"); // if just looking at pt files
+        if (Files.isDirectory(path)) {
+            return Files.exists(path.resolve("instanseg.pt")) && Files.exists(path.resolve("rdf.yaml"));
+        }
+        return false;
+    }
+
+
+}
