d9f55244970c7eba8d0ec5804ca028bacdcfba46
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2025-10-17 16:26:45 +0100

    v0.1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 qupathExtension {
     name = "qupath-extension-instanseg"
-    version = "0.1.4"
+    version = "0.1.5"
     group = "io.github.qupath"
     description = "A QuPath extension for running inference with the InstanSeg deep learning model"
     automaticModule = "qupath.extension.instanseg"
